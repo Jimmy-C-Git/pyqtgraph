37a1e2fc2ff0616af7c8abe260aba920dc1547e6
Jimmy_Windows <caojin0405@qq.com>
2019-05-20 22:05:36 +0800

    On branch dev_jimmy Changes to be committed: 	modified:   pyqtgraph/widgets/DataTreeWidget.py

--- pyqtgraph/widgets/DataTreeWidget.py
+++ pyqtgraph/widgets/DataTreeWidget.py
@@ -36,89 +36,120 @@ class DataTreeWidget(QtGui.QTreeWidget):
         self.expandToDepth(3)
         self.resizeColumnToContents(0)
         
-    def buildTree(self, data, parent, name='', hideRoot=False, path=()):
+    def buildTree(self, data, parent, name='', hideRoot=False):
         if hideRoot:
             node = parent
         else:
-            node = QtGui.QTreeWidgetItem([name, "", ""])
+            typeStr = type(data).__name__
+            if typeStr == 'instance':
+                typeStr += ": " + data.__class__.__name__
+            node = QtGui.QTreeWidgetItem([name, typeStr, ""])
             parent.addChild(node)
-        
-        # record the path to the node so it can be retrieved later
-        # (this is used by DiffTreeWidget)
-        self.nodes[path] = node
 
-        typeStr, desc, childs, widget = self.parse(data)
-        node.setText(1, typeStr)
-        node.setText(2, desc)
-            
-        # Truncate description and add text box if needed
-        if len(desc) > 100:
-            desc = desc[:97] + '...'
-            if widget is None:
-                widget = QtGui.QPlainTextEdit(asUnicode(data))
-                widget.setMaximumHeight(200)
-                widget.setReadOnly(True)
-        
-        # Add widget to new subnode
-        if widget is not None:
-            self.widgets.append(widget)
-            subnode = QtGui.QTreeWidgetItem(["", "", ""])
-            node.addChild(subnode)
-            self.setItemWidget(subnode, 0, widget)
-            self.setFirstItemColumnSpanned(subnode, True)
-            
-        # recurse to children
-        for key, data in childs.items():
-            self.buildTree(data, node, asUnicode(key), path=path+(key,))
-
-    def parse(self, data):
-        """
-        Given any python object, return:
-        * type
-        * a short string representation
-        * a dict of sub-objects to be parsed
-        * optional widget to display as sub-node
-        """
-        # defaults for all objects
-        typeStr = type(data).__name__
-        if typeStr == 'instance':
-            typeStr += ": " + data.__class__.__name__
-        widget = None
-        desc = ""
-        childs = {}
-        
-        # type-specific changes
-        if isinstance(data, dict):
-            desc = "length=%d" % len(data)
-            if isinstance(data, OrderedDict):
-                childs = data
-            else:
-                childs = OrderedDict(sorted(data.items()))
-        elif isinstance(data, (list, tuple)):
-            desc = "length=%d" % len(data)
-            childs = OrderedDict(enumerate(data))
+        if isinstance(data, types.TracebackType):  ## convert traceback to a list of strings
+            data = list(map(str.strip, traceback.format_list(traceback.extract_tb(data))))
         elif HAVE_METAARRAY and (hasattr(data, 'implements') and data.implements('MetaArray')):
-            childs = OrderedDict([
-                ('data', data.view(np.ndarray)),
-                ('meta', data.infoCopy())
-            ])
-        elif isinstance(data, np.ndarray):
-            desc = "shape=%s dtype=%s" % (data.shape, data.dtype)
-            table = TableWidget()
-            table.setData(data)
-            table.setMaximumHeight(200)
-            widget = table
-        elif isinstance(data, types.TracebackType):  ## convert traceback to a list of strings
-            frames = list(map(str.strip, traceback.format_list(traceback.extract_tb(data))))
-            #childs = OrderedDict([
-                #(i, {'file': child[0], 'line': child[1], 'function': child[2], 'code': child[3]})
-                #for i, child in enumerate(frames)])
-            #childs = OrderedDict([(i, ch) for i,ch in enumerate(frames)])
-            widget = QtGui.QPlainTextEdit(asUnicode('\n'.join(frames)))
-            widget.setMaximumHeight(200)
-            widget.setReadOnly(True)
+            data = {
+                'data': data.view(np.ndarray),
+                'meta': data.infoCopy()
+            }
+            
+        if isinstance(data, dict):
+            for k in data.keys():
+                self.buildTree(data[k], node, str(k))
+        elif isinstance(data, list) or isinstance(data, tuple):
+            for i in range(len(data)):
+                self.buildTree(data[i], node, str(i))
         else:
-            desc = asUnicode(data)
+            node.setText(2, str(data))    
+
+     
+
+    'use followed buildTree can not show too big arrays or to many data'
+    'TODO: can load new widget when expand'
+    # def buildTree(self, data, parent, name='', hideRoot=False, path=()):
+    #     if hideRoot:
+    #         node = parent
+    #     else:
+    #         node = QtGui.QTreeWidgetItem([name, "", ""])
+    #         parent.addChild(node)
         
-        return typeStr, desc, childs, widget
+    #     # record the path to the node so it can be retrieved later
+    #     # (this is used by DiffTreeWidget)
+    #     self.nodes[path] = node
+
+    #     typeStr, desc, childs, widget = self.parse(data)
+    #     node.setText(1, typeStr)
+    #     node.setText(2, desc)
+            
+    #     # Truncate description and add text box if needed
+    #     if len(desc) > 100:
+    #         desc = desc[:97] + '...'
+    #         if widget is None:
+    #             widget = QtGui.QPlainTextEdit(asUnicode(data))
+    #             widget.setMaximumHeight(200)
+    #             widget.setReadOnly(True)
+        
+    #     # Add widget to new subnode
+    #     if widget is not None:
+    #         self.widgets.append(widget)
+    #         subnode = QtGui.QTreeWidgetItem(["", "", ""])
+    #         node.addChild(subnode)
+    #         self.setItemWidget(subnode, 0, widget)
+    #         self.setFirstItemColumnSpanned(subnode, True)
+            
+    #     # recurse to children
+    #     for key, data in childs.items():
+    #         self.buildTree(data, node, asUnicode(key), path=path+(key,))
+
+    # def parse(self, data):
+    #     """
+    #     Given any python object, return:
+    #     * type
+    #     * a short string representation
+    #     * a dict of sub-objects to be parsed
+    #     * optional widget to display as sub-node
+    #     """
+    #     # defaults for all objects
+    #     typeStr = type(data).__name__
+    #     if typeStr == 'instance':
+    #         typeStr += ": " + data.__class__.__name__
+    #     widget = None
+    #     desc = ""
+    #     childs = {}
+        
+    #     # type-specific changes
+    #     if isinstance(data, dict):
+    #         desc = "length=%d" % len(data)
+    #         if isinstance(data, OrderedDict):
+    #             childs = data
+    #         else:
+    #             childs = OrderedDict(sorted(data.items()))
+    #     elif isinstance(data, (list, tuple)):
+    #         desc = "length=%d" % len(data)
+    #         childs = OrderedDict(enumerate(data))
+    #     elif HAVE_METAARRAY and (hasattr(data, 'implements') and data.implements('MetaArray')):
+    #         childs = OrderedDict([
+    #             ('data', data.view(np.ndarray)),
+    #             ('meta', data.infoCopy())
+    #         ])
+    #     elif isinstance(data, np.ndarray):
+    #         desc = "shape=%s dtype=%s" % (data.shape, data.dtype)
+    #         table = TableWidget()
+    #         table.setData(data)
+    #         table.setMaximumHeight(200)
+    #         widget = table
+    #     elif isinstance(data, types.TracebackType):  ## convert traceback to a list of strings
+    #         frames = list(map(str.strip, traceback.format_list(traceback.extract_tb(data))))
+    #         #childs = OrderedDict([
+    #             #(i, {'file': child[0], 'line': child[1], 'function': child[2], 'code': child[3]})
+    #             #for i, child in enumerate(frames)])
+    #         #childs = OrderedDict([(i, ch) for i,ch in enumerate(frames)])
+    #         widget = QtGui.QPlainTextEdit(asUnicode('\n'.join(frames)))
+    #         widget.setMaximumHeight(200)
+    #         widget.setReadOnly(True)
+    #     else:
+    #         desc = asUnicode(data)
+        
+    #     return typeStr, desc, childs, widget
         
